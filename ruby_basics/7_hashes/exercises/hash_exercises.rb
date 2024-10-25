97836cd7fb2d219f0265bdce105dee6d61a5f148
jrsweig9021 <jrsweig9021@gmail.com>
2024-10-24 21:38:54 -0400

    progress update: 7_hashes exercise completed

--- ruby_basics/7_hashes/exercises/hash_exercises.rb
+++ ruby_basics/7_hashes/exercises/hash_exercises.rb
@@ -2,7 +2,7 @@ def create_favorite_hash(color, number)
   # return a hash with the following key/value pairs:
   # key of color (as a symbol) with value of the color argument
   # key of number (as a symbol) with the value of the number argument
-  Hash.new({ :color => color, :number => number })
+  { :color => color, :number => number }
 end
 
 def favorite_color(favorite_list)
@@ -24,19 +24,22 @@ end
 
 def remove_favorite_number(favorite_list)
   # Step 1: delete the number data
-
+  favorite_list.delete(:number)
   # Step 2: return the hash (because Step 1 returns the value of the number key)
   favorite_list
 end
 
 def favorite_categories(favorite_list)
   # return the keys of favorite_list
+  favorite_list.keys
 end
 
 def favorite_items(favorite_list)
   # return the values of favorite_list
+  favorite_list.values
 end
 
 def merge_favorites(original_list, additional_list)
   # merge the two hashes: original_list and additional_list
+  original_list.merge(additional_list)
 end
